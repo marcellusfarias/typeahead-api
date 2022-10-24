4a2f29183047037f641609426425129772a21642
Marcellus Farias <mafarias@concordservicing.com>
2022-10-24 10:28:47 -0300

    Increase popularity function working (needs error handling)

--- src/trie.rs
+++ src/trie.rs
@@ -60,19 +60,14 @@ impl ITrie for Trie {
 
     //return RESULT
     //remove unwrap
-    //change for loop
     fn increase_popularity(&mut self, word: String) {
         let mut node = &mut self.root;
-        let len = word.len();
-        let i = 0;
 
         for char in word.chars() {
-            if i == len - 1 {
-                node.value = Some(node.value.unwrap() + 1);
-            }
-
             node = node.children.get_mut(&char).unwrap();
         }
+
+        node.value = Some(node.value.unwrap_or(0) + 1);
     }
 
     fn search_with_prefix(&mut self, prefix: String) -> Vec<(String, u16)> {
@@ -99,6 +94,8 @@ impl TrieNode {
 
 #[cfg(test)]
 mod tests {
+    use serde::__private::de;
+
     use super::*;
 
     fn compare_tries(root_a: &Box<TrieNode>, root_b: &Box<TrieNode>) -> bool {
@@ -137,13 +134,7 @@ mod tests {
         }
     }
 
-    #[test]
-    fn t_initialize_data() {
-        let file_content =
-            "{\"Aar\":361,\"Aari\":151,\"Aba\":704,\"Abag\":608, \"Abe\": 300, \"Ba\": 5, \"Be\": 50}";
-        let trie = Trie::initialize(file_content, 10);
-
-        // constructing expected trie
+    fn initialize_testing_trie() -> Trie {
         let mut expected_trie = Trie {
             root: Box::new(TrieNode::new(' ', None)),
             suggestion_number: 10,
@@ -204,7 +195,80 @@ mod tests {
         node.children
             .insert('e', Box::new(TrieNode::new('e', Some(50))));
 
-        // let mut level = 0;
+        expected_trie
+    }
+
+    fn increase_popularity_testing_trie() -> Trie {
+        let mut expected_trie = Trie {
+            root: Box::new(TrieNode::new(' ', None)),
+            suggestion_number: 10,
+        };
+
+        let mut node: &mut Box<TrieNode> = &mut expected_trie.root;
+
+        // (A) first level
+        node.children
+            .insert('A', Box::new(TrieNode::new('A', None)));
+
+        // (A) second level
+        node = node.children.get_mut(&'A').unwrap();
+        node.children
+            .insert('a', Box::new(TrieNode::new('a', None)));
+        node.children
+            .insert('b', Box::new(TrieNode::new('b', None)));
+
+        // (A) third level
+        node = node.children.get_mut(&'a').unwrap();
+        node.children
+            .insert('r', Box::new(TrieNode::new('r', Some(361))));
+
+        // (A) fourth level
+        node = node.children.get_mut(&'r').unwrap();
+        node.children
+            .insert('i', Box::new(TrieNode::new('i', Some(151))));
+
+        // (A) third level
+        node = &mut expected_trie.root;
+        node = node.children.get_mut(&'A').unwrap();
+        node = node.children.get_mut(&'b').unwrap();
+
+        node.children
+            .insert('a', Box::new(TrieNode::new('a', Some(704))));
+
+        // (A) fourth level
+        node = node.children.get_mut(&'a').unwrap();
+        node.children
+            .insert('g', Box::new(TrieNode::new('g', Some(608))));
+
+        // (A) third level
+        node = &mut expected_trie.root;
+        node = node.children.get_mut(&'A').unwrap();
+        node = node.children.get_mut(&'b').unwrap();
+        node.children
+            .insert('e', Box::new(TrieNode::new('e', Some(301))));
+
+        // (B) first level
+        node = &mut expected_trie.root;
+        node.children
+            .insert('B', Box::new(TrieNode::new('B', None)));
+
+        // (B) second level
+        node = node.children.get_mut(&'B').unwrap();
+        node.children
+            .insert('a', Box::new(TrieNode::new('a', Some(5))));
+        node.children
+            .insert('e', Box::new(TrieNode::new('e', Some(50))));
+
+        expected_trie
+    }
+    #[test]
+    fn t_initialize() {
+        let file_content =
+            "{\"Aar\":361,\"Aari\":151,\"Aba\":704,\"Abag\":608, \"Abe\": 300, \"Ba\": 5, \"Be\": 50}";
+        let trie = Trie::initialize(file_content, 10);
+
+        let expected_trie = initialize_testing_trie();
+
         print_trie(&expected_trie.root, 0);
         print_trie(&trie.root, 0);
 
@@ -215,71 +279,17 @@ mod tests {
     fn t_insert_word_into_db() {}
 
     #[test]
-    fn t_update_popularity_word() {}
+    fn t_increase_popularity() {
+        let file_content =
+        "{\"Aar\":361,\"Aari\":151,\"Aba\":704,\"Abag\":608, \"Abe\": 300, \"Ba\": 5, \"Be\": 50}";
+        let mut trie = Trie::initialize(file_content, 10);
+        trie.increase_popularity("Abe".to_string());
 
-    #[test]
-    fn t_insert_word_into_trie() {
-        let mut trie = Trie {
-            root: Box::new(TrieNode {
-                children: HashMap::new(),
-                letter: ' ',
-                value: None,
-            }),
-            suggestion_number: 10,
-        };
-        let mut node = &mut trie.root;
+        let expected_trie = increase_popularity_testing_trie();
 
-        let word = "RUST";
-        let popularity = 100;
-        let len = word.len();
-        let mut i = 0;
+        // print_trie(&trie.root, 0);
+        // print_trie(&expected_trie.root, 0);
 
-        for char in word.chars() {
-            let value: Option<u16> = if i == len - 1 { Some(popularity) } else { None };
-
-            let new_node = Box::new(TrieNode {
-                children: HashMap::new(),
-                letter: char.clone(),
-                value: value,
-            });
-
-            if !node.children.contains_key(&char) {
-                node.children.insert(char, new_node.clone());
-            }
-
-            node = node.children.get_mut(&char).unwrap();
-
-            i = i + 1;
-
-            println!("[{}] new_node: {:?}", i, new_node);
-        }
-
-        let mut node = &mut trie.root;
-        let word = "RUDE";
-        let popularity = 90;
-        let len = word.len();
-        let mut i = 0;
-
-        for char in word.chars() {
-            let value: Option<u16> = if i == len - 1 { Some(popularity) } else { None };
-
-            let new_node = Box::new(TrieNode {
-                children: HashMap::new(),
-                letter: char.clone(),
-                value: value,
-            });
-
-            if !node.children.contains_key(&char) {
-                node.children.insert(char, new_node.clone());
-            }
-
-            node = node.children.get_mut(&char).unwrap();
-
-            i = i + 1;
-
-            println!("[{}] new_node: {:?}", i, new_node);
-        }
-
-        println!("tree: {:?}", trie);
+        assert!(compare_tries(&trie.root, &expected_trie.root));
     }
 }
